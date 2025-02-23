3058c9338cb7944df4b7f7557e78d89b8be0d106
macygmac <mgm5927@psu.edu>
2025-02-22 20:05:35 -0500

    homework 6 commit

--- counter-app.js
+++ counter-app.js
@@ -20,6 +20,9 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
 
   constructor() {
     super();
+    this.count = 5;
+    this.max = 25;
+    this.min = 0;
     this.title = "";
     this.t = this.t || {};
     this.t = {
@@ -39,7 +42,9 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
   static get properties() {
     return {
       ...super.properties,
-      title: { type: String },
+      count: { type: Number, reflect: true },
+      max: { type: Number, reflect: true },
+      min: { type: Number, reflect: true },
     };
   }
 
@@ -48,28 +53,119 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
     return [super.styles,
     css`
       :host {
-        display: block;
+        display: inline-block;
         color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
+        background-color: var(--ddd-theme-default-alertImmediate);
         font-family: var(--ddd-font-navigation);
+       
       }
+      
+      :host([count="0"]),
+      :host([count="10"]),
+      :host([count="25"]) {
+        color: var(--ddd-theme-default-creekTeal);
+      }
+      :host([count="18"]),
+      :host([count="21"]) {
+        color: var(--ddd-theme-default-original87Pink);
+      }  
+      
       .wrapper {
         margin: var(--ddd-spacing-2);
         padding: var(--ddd-spacing-4);
+        
       }
       h3 span {
         font-size: var(--counter-app-label-font-size, var(--ddd-font-size-s));
       }
+      .counter {
+        font-size: var(--counter-app-counter-font-size, var(--ddd-font-size-xxl));
+        font-weight: bold;
+        text-align: center;
+        
+      }
+      button:hover {
+        background-color: var(--ddd-theme-default-original87Pink);
+        color: var(--ddd-theme-default-creekMaxLight);    
+
+    }
+    button {
+      padding: var(--ddd-spacing-1);
+      margin: var(--ddd-spacing-1);
+      
+    }
+
+       
+
     `];
   }
+
+
+
+  updated(changedPropertied) {
+    super.updated(changedPropertied);
+    if (changedPropertied.has("count")) {
+      console.log("Count updated to", this.count);
+      if (this.count === 21) {
+        this.makeItRain();
+      }
+    }
+  }
+
+
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
+  }
+
+
 
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+<confetti-container id="confetti" class="wrapper">
+  <div class="counter">${this.count}</div>
+  <div class="buttons">
+    <button @click="${this.decrease}" ?disabled="${this.isMin()}">-1</button>
+    <button @click="${this.increase}" ?disabled="${this.isMax()}">+1</button>
+  </div>
+  </confetti-container>
+  `;
+  }
+
+  isMax() {
+    return this.count === this.max;
+  }
+
+  isMin() {
+    return this.count === this.min;
+  }
+
+  increase() {
+    this.count++;
+  }
+
+  decrease() {
+    this.count--;
+  }
+
+  reset() {
+    this.count = 0;
   }
 
   /**
